9be84dc75dc444ac483b035cd4f954e2becbade0
Devarsh <devarsh600@gmail.com>
2024-09-16 08:48:48 +0530

    Final Commit -v1 Completed

--- src/schemas/emailSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const emailSchema = z
-  .string()
-  .email({ message: "Please enter a valid email adderess" })
-  .regex(
-    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    "Please enter a valid email adderess"
-  );
